fd142a45afb18440e535ba0b312d27fe51bf711c
MykhayloOdesa <m.yervachov@gmail.com>
2022-12-22 20:28:15 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,7 +15,7 @@
 // Використовуй готовий код з першого завдання.
 // Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs.
 // Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
-// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery.
 // Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
 // Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
 // Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
@@ -24,3 +24,45 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// console.dir(SimpleLightbox);
+
+const galleryRef = document.querySelector(".gallery");
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Використовуй готовий код з першого завдання.
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+  )
+  .join("");
+
+galleryRef.innerHTML = markup;
+
+// Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery.
+// Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+galleryRef.addEventListener("click", onPictureClick);
+
+function onPictureClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
+  // Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+  let lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  lightbox.on("closed.simplelightbox", () => {
+    lightbox.close();
+    lightbox.refresh();
+  });
+}
